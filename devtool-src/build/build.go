91ae31d9ef2cac09d5e489417fa1914a14d52a38
Jordi Izquierdo <jordi.izquierdo@aizon.ai>
2024-07-19 17:20:15 +0200

    svg loader

--- devtool-src/build/build.go
+++ devtool-src/build/build.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"path/filepath"
+	"regexp"
 
 	"github.com/evanw/esbuild/pkg/api"
 	"github.com/tdewolff/minify/v2"
@@ -11,12 +13,39 @@ import (
 	"github.com/tdewolff/minify/v2/html"
 )
 
+const distPath = "/dist"
+const regexSvgPattern = `<img[^>]+src="([^"]+\.svg)"`
+
+func handleSvgAssets(content []byte) (api.OnLoadResult, error) {
+	regex, err := regexp.Compile(regexSvgPattern)
+	if err != nil {
+		fmt.Println("Error compiling regex:", err)
+		return api.OnLoadResult{}, err
+	}
+
+	matches := regex.FindAllStringSubmatch(string(content), -1)
+
+	for _, match := range matches {
+		if len(match) > 1 { // match[0] is the full match, match[1] is the first capture group
+			cleanPath := filepath.Base(match[1])
+			finalPath := filepath.Join(distPath, cleanPath)
+
+			fmt.Println("Found .svg file:", match[1])
+			fmt.Println("Modified .svg file path:", finalPath)
+		}
+	}
+
+	return api.OnLoadResult{}, nil
+}
+
 func loadAndMinify(m *minify.M, fileType string) func(args api.OnLoadArgs) (api.OnLoadResult, error) {
 	return func(args api.OnLoadArgs) (api.OnLoadResult, error) {
 		content, err := os.ReadFile(args.Path)
 		if err != nil {
 			return api.OnLoadResult{}, err
 		}
+
+		handleSvgAssets(content)
 
 		minified, err := m.String(fileType, string(content))
 		if err != nil {
@@ -41,7 +70,7 @@ func Run() {
 		MinifyWhitespace:  false,
 		MinifyIdentifiers: false,
 		MinifySyntax:      false,
-		Outdir:            "dist",
+		Outdir:            distPath,
 		Write:             true,
 		Loader: map[string]api.Loader{
 			".html": api.LoaderText,
@@ -68,14 +97,4 @@ func Run() {
 	if len(result.Errors) > 0 {
 		log.Fatal(result.Errors)
 	}
-
-	fmt.Println(`
-    ########::: ######::: ##::: ##: ########: ####:
-    ##.... ##: ##.... ##: ###:: ##: ##.....:: ####:
-    ##:::: ##: ##:::: ##: ####: ##: ##::::::: ####:
-    ##:::: ##: ##:::: ##: ## ## ##: ######:::: ##::
-    ##:::: ##: ##:::: ##: ##. ####: ##...:::::..:::
-    ##:::: ##: ##:::: ##: ##:. ###: ##:::::::'####:
-    ########::. #######:: ##::. ##: ########: ####:
-    ........::::.......:::..::::..::........::....:`)
 }
