692edd454590542d814c05fe21f8c701fec2c5d4
Jordi <jordi.izquierdo.casares@gmail.com>
2024-08-03 22:49:06 +0200

    tooltip

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,7 @@
 import { SwitchableTable } from './switchable-table';
 import { CustomTable } from './custom-table';
+import { Tooltip } from './tooltip';
 
 customElements.define("switchable-table", SwitchableTable);
 customElements.define("custom-table", CustomTable);
+customElements.define("custom-tooltip", Tooltip);
